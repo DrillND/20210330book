352d743d7d799f5da7521ef82955bbae3347942c
jinsim88 <81124904+jinsim88@users.noreply.github.com>
2021-03-30 09:30:49 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p267
+{
+    class Test
+    {
+        public int Multi(int x, int y)
+        {
+            return x * y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("print() 메서드가 호출되었습니다.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Console.WriteLine(test.Multi(52, 273));
+            Console.WriteLine(test.Multi(103, 32));
+            test.Print();
+        }
+    }
+}
